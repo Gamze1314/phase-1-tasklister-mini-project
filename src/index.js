8f3053b9d3f151b8e2bd2f39e0858acb1c0b9c70
gamze <gamzekandara@gmail.com>
2023-11-27 15:54:05 -0500

    Adds index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
+  console.log(document.querySelector('#create-task-form'))
   // your code here
+// we will add x button to remove
+  // add text and submit ( submit event)
+  document.querySelector('#create-task-form').addEventListener('submit' ,(e) => { console.log('task')
+    e.preventDefault() // prevents default action
+    console.log(e.target["new-task-description"].value)
+    handleTaskLister(e.target["new-task-description"].value) //the value you type in will be handled by handleTaskLister function
+    
+  })
+
 });
+
+function handleTaskLister (task) {
+  let p = document.createElement('li')
+  let btn = document.createElement('button') 
+  btn.addEventListener('click' , deleter) // should I add eventlistener in tihs scope ? 
+
+  btn.textContent = ' x'
+  p.textContent = task
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+
+function deleter (e) { 
+  e.target.parentNode.remove() // we grabbed the target of event = button , why we grab the parent node ?
+
+}
+
